9855f73f6ebb29b12639f9d3a75c9eed42855b68
Zaidong Yang <zaidongy@gmail.com>
2018-10-23 14:09:47 -0700

    production code fix

--- server.js
+++ server.js
@@ -6,16 +6,6 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
 const items = require('./routes/api/items');
 
 // Database Connection
@@ -27,5 +17,15 @@ mongoose
 
 //use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.listen(config.port, () => console.log(`Server started on port ${config.port}`));
