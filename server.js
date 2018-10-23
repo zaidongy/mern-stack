77dd979334f73c0ab5e0b0211504ba37a3d9ea9c
Zaidong Yang <zaidongy@gmail.com>
2018-10-23 13:07:06 -0700

    added production scripts and production endpoitn

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Setup Server
 const app = express();
 app.use(express.json());
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const items = require('./routes/api/items');
 
